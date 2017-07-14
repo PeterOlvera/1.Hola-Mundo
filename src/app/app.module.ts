c2b9b4d25a89e6cffcfa199a470f36291394b2ad
Olvera Quisilay <r.olvera.quisilay@accenture.com>
2017-07-14 14:10:23 +0200

    nuevo contenido en body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule
