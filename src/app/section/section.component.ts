804b3fbdfca6bf0637c5f6b92f5c47e755ad6968
Olvera Quisilay <r.olvera.quisilay@accenture.com>
2017-07-14 14:24:15 +0200

    funcionalidades angular a section

--- src/app/section/section.component.ts
+++ src/app/section/section.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-section',
   templateUrl: './section.component.html',
   styleUrls: ['./section.component.css']
 })
-export class SectionComponent implements OnInit {
+export class SectionComponent {
+  mostrar:boolean=false;
 
-  constructor() { }
-
-  ngOnInit() {
+  frase:any = {
+    mensaje: "Un gran poder conlleva una gran responsabilidad",
+    autor: "Ben Parker"
   }
+
+  personajes:string[] = ["Spiderman", "Capitan america", "Hulk"];
 
 }
